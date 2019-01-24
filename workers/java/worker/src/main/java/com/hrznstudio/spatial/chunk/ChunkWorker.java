fbe5e08cada5dff27668f4a0899912acfd5737cf
bluexin <bluexin.games@gmail.com>
2019-01-24 17:45:50 +0100

    Revamp chunk schema
    #14 Chunk Syncing

--- workers/java/worker/src/main/java/com/hrznstudio/spatial/chunk/ChunkWorker.java
+++ workers/java/worker/src/main/java/com/hrznstudio/spatial/chunk/ChunkWorker.java
@@ -1,6 +1,12 @@
 package com.hrznstudio.spatial.chunk;
 
 import com.hrznstudio.spatial.util.BaseWorker;
+import com.hrznstudio.spatial.util.CommonWorkerRequirements;
+import com.hrznstudio.spatial.util.EntityBuilder;
+import improbable.Position;
+import improbable.worker.*;
+import minecraft.world.ChunkStorage;
+import minecraft.world.ChunkStorageData;
 
 public class ChunkWorker extends BaseWorker {
     @Override
@@ -8,4 +14,23 @@ public class ChunkWorker extends BaseWorker {
         return "ChunkWorker";
     }
 
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+    public static void createChunk(Dispatcher dispatcher, Connection connection) {
+        final improbable.collections.Option<Integer> timeoutMillis = improbable.collections.Option.of(500);
+
+        // Reserve an entity ID.
+        RequestId<ReserveEntityIdRequest> entityIdReservationRequestId = connection.sendReserveEntityIdRequest(timeoutMillis);
+        // When the reservation succeeds, create an entity with the reserved ID.
+
+        dispatcher.onReserveEntityIdResponse(op -> {
+            if (op.requestId.equals(entityIdReservationRequestId) && op.statusCode == StatusCode.SUCCESS) {
+                EntityBuilder builder = new EntityBuilder("chunk");
+                builder.addComponent(ChunkStorage.COMPONENT, ChunkStorageData.create(), CommonWorkerRequirements.getAllCommonWorkers());
+                builder.addComponent(Position.COMPONENT, new improbable.PositionData(new improbable.Coordinates(1, 2, 3)), CommonWorkerRequirements.getAllCommonWorkers());
+            }
+        });
+    }
 }
